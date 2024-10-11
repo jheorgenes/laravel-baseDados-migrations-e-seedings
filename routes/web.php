c9bff3ba28d5a1d52a44ea697dbfd57581ebf5d2
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-10-11 10:56:26 -0300

    -- Aprendendo a utilizar várias bases de dados com o Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,34 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/', function () {
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        die("Não foi possível conectar com a base de dados. Erro: " . $e->getMessage());
+    }
+});
+
+Route::get('/sqlite', function () {
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        die("Não foi possível conectar com a base de dados. Erro: " . $e->getMessage());
+    }
+});
+
+Route::get('/mysql_test_two_databases', function () {
+    try {
+        DB::connection('mysql_users')->getPdo();
+        DB::connection('mysql_app')->getPdo();
+        echo "Conexão com a base de dados: " . DB::connection('mysql_users')->getDatabaseName();
+        echo "<br>";
+        echo "Conexão com a base de dados: " . DB::connection('mysql_app')->getDatabaseName();
+    } catch (\Exception $e) {
+        die("Não foi possível conectar com a base de dados. Erro: " . $e->getMessage());
+    }
+});
